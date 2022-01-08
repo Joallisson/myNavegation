8474a518b7d268450cfadcde34de75a0370bad5d
Joallisson <joallissons@gmail.com>
2022-01-08 19:09:00 -0300

    Finalizado app de navegação
    Versão 1

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -1,10 +1,40 @@
 import React from "react";
 import {View, Text} from 'react-native'
 
-export default function Information(){
+export default function Contacts({navigation}){
     return(
         <View>
-            <Text>Contacts</Text>
+            <View style = {{paddingTop: 60}}>
+                <Text>Nome: João Silva</Text>
+                <Text>Telefone: (99) 99999-9999</Text>
+                <Text
+                onPress = {() => navigation.navigate('Information', 
+                {
+                    nome: 'João Silva',
+                    telefone: '(99) 99999-9999',
+                    endereco: 'Rua das flores',
+                    numero: '765',
+                    profissao: 'Carpinteiro',
+                    email: 'joaosilva@carpintaria.com.br',
+                })}
+                >Information...</Text>
+            </View>
+
+            <View style = {{paddingTop: 60}}>
+                <Text>Nome: Amanda de Alencar</Text>
+                <Text>Telefone: (00) 00000-0000</Text>
+                <Text
+                onPress = {() => navigation.navigate('Information', 
+                {
+                    nome: 'Amanda de Alencar',
+                    telefone: '(00) 00000-0000',
+                    endereco: 'Av. estrela nova',
+                    numero: '3658',
+                    profissao: 'Desenvolvedora de Softwares',
+                    email: 'amanda.dev@amandadev.com.br',
+                })}
+                >Information </Text>
+            </View>
         </View>
     )
 }
--- src/pages/Information/index.js
+++ src/pages/Information/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import {View, Text} from 'react-native'
 
-export default function Contacts(){
+export default function Information({route}){
     return(
         <View>
-            <Text>Information</Text>
+            <Text>Nome: {route.params?.nome}</Text>
+            <Text>Telefone: {route.params?.telefone}</Text>
+            <Text>Endereço: {route.params?.endereco}</Text>
+            <Text>Nº: {route.params?.numero}</Text>
+            <Text>Profissão: {route.params?.profissão}</Text>
+            <Text>Email: {route.params?.email}</Text>
         </View>
     )
 }
